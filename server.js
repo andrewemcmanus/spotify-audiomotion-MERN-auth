eea8309fb715dba76b85686835f4138005328509
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-04-05 09:42:31 -0400

    update [server.js]

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // const createError = require('http-errors');
+require('dotenv').config();
 const express = require('express');
 // var path = require('path');
 // const cookieParser = require('cookie-parser');
@@ -6,8 +7,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const passport = require('passport');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// const indexRouter = require('./routes/index');
+// const usersRouter = require('./routes/users');
 const PORT = process.env.PORT || 8000;
 
 //  api
@@ -18,14 +19,19 @@ const request = require('./api/request');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json())
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Connected to back end'})
+});
+
+app.use('/api/users', users);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
