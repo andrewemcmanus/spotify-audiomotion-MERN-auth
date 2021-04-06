9aefd3e200654862e071999d4a84737c2961947a
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-04-06 07:56:01 -0400

    [bcryptjs]

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ const PORT = process.env.PORT || 8000;
 
 //  api
 const users = require('./api/users');
-const request = require('./api/request');
+// const request = require('./api/request');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // app.use(logger('dev'));
 app.use(express.json());
