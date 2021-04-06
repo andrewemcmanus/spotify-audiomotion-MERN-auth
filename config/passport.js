60cd9f421895010dfd1355a7ac23a1129d0bddda
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-04-06 08:32:08 -0400

    [models] add mongoose

--- config/passport.js
+++ config/passport.js
@@ -4,3 +4,7 @@ require('dotenv').config();
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
+
+const User = require('../models/User');
+
+const options = {};
---
+++ models/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+});
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+});
+
+db.on('error', (error) => {
+  console.log(`Database error\n ${error}`);
+});
+
+module.exports.User = require('./User');
