65ba1de8bf0eaf3e19e6959c08b20ae9e4e0fd09
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-04-07 15:49:37 -0400

    [server] cors middleware

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 // const session = require('express-session');
 const passport = require('passport');
 require('./config/passport')(passport)
-const SECRET_SESSION = process.env.CLIENT_SECRET;
+// const SECRET_SESSION = process.env.CLIENT_SECRET;
 // const indexRouter = require('./routes/index');
 // const usersRouter = require('./routes/users');
 const PORT = process.env.PORT || 8000;
@@ -23,11 +23,11 @@ const users = require('./api/users');
 
 // MIDDLEWARE
 // app.use(logger('dev'));
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json())
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Connected to back end'});
@@ -36,21 +36,22 @@ app.get('/', (req, res) => {
 app.use('/api/users', users);
 // app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// ERROR HANDLERS: NECESSARY HERE?
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is live on port ${PORT}`);
