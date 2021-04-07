60de944910e0465df216cb47461fcc3dafbb3ebf
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-04-07 10:41:18 -0400

    [api] add POST request for /register

--- api/users.js
+++ api/users.js
@@ -4,12 +4,44 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
-// const JWT_SECRET = process.env.JWT_SECRET
-// cost db = require('../models')
+const JWT_SECRET = process.env.JWT_SECRET
+const db = require('../models')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/test', (req, res) => {
+  res.json({ message: 'User endpoint' })
+})
+
+router.post('/register', (req, res) => {
+  db.User.findOne({ email: req.body.email })
+  .then(user => {
+    if (user) {
+      return res.status(400).json({ msg: 'Email is already registered'})
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      })
+
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) throw Error;
+        bcrypt.hasn(newUser.password, salt, (error, hash) => {
+          if (error) throw Error;
+          newUser.password = hash;
+          newUser.save()
+          .then(createdUser => res.json(createdUser))
+          .catch(err => console.log(err))
+        })
+      })
+    }
+  })
+})
+
+
 
 module.exports = router;
